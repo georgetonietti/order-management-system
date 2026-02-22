3b80ec446669589389f7e063504295e4b9e50a1c
georgetonietti <georgetonietti@gmail.com>
2026-02-21 22:50:56 -0300

    feat: product service wip

---
+++ product-service/src/main/java/com/dev/product_service/domain/Product.java
@@ -0,0 +1,30 @@
+package com.dev.product_service.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private Integer stock;
+
+    @
+    private LocalDateTime createdAt;
+}
